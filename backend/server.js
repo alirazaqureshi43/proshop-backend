d252fef6d28f85bc8f825fe3733fdc7a2b146cc8
Ali Raza <alirazaqureshi43@gmail.com>
2024-07-05 19:43:01 +0500

    changes

--- backend/server.js
+++ backend/server.js
@@ -27,15 +27,9 @@ app.use('/api/upload', uploadRoutes)
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname, '/frontend/build')))
-    app.get('*',(res,req)=> res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
-}else{
-    app.get('/',(req,res)=>{
-        res.send('Api running...')
-    })
-}
-
+app.get('/',(req,res)=>{
+    res.send('Api running...')
+})
 app.use(notFound)
 app.use(errorHandler)
 
