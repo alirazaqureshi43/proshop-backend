36b406002a98dcce9ddac7aadc7595045002fcbe
Ali Raza <alirazaqureshi43@gmail.com>
2024-08-10 02:53:15 +0500

    frontend-added

---
+++ frontend/src/constants.js
@@ -0,0 +1,6 @@
+export const BASE_URL = 'https://proshop-backend-mu.vercel.app';
+export const PRODUCTS_URL = 'https://proshop-backend-mu.vercel.app/api/products';
+export const UPLOAD_URL = 'https://proshop-backend-mu.vercel.app/api/upload';
+export const USERS_URL = 'https://proshop-backend-mu.vercel.app/api/users';
+export const ORDERS_URL = 'https://proshop-backend-mu.vercel.app/api/orders';
+export const PAYPAL_URL = 'https://proshop-backend-mu.vercel.app/api/config/paypal';
